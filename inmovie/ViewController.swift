df183cdb8ed1b21481970f091ec889e3792a639e
jadenjoy <dailynews.rus@gmail.com>
2018-11-27 20:45:42 +0300

    Initial Commit

---
+++ inmovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inmovie
+//
+//  Created by Константин on 27/11/2018.
+//  Copyright © 2018 Константин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
